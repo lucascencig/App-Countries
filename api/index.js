a43e2b2c701bc088b024a6761128b595194a5a33
lucascencig <cenciglucas@gmail.com>
2022-06-22 20:54:00 -0300

    back terminado 2

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn, Country } = require('./src/db.js');
-const loadDB = require('../api/src/loadDBase/loadDBase');
+const loadDB = require('../api/src/loadDBase/loadDBase.js');
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
