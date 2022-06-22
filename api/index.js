fe399518b1566baa6c7877f01d6891beb7e39f82
lucascencig <cenciglucas@gmail.com>
2022-06-22 20:56:42 -0300

    back terminado 2

--- api/index.js
+++ api/index.js
@@ -19,13 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn, Country } = require('./src/db.js');
-const loadDB = require('../api/src/loadDBase/loadDBase.js');
+// const loadDB = require('../api/src/loadDBase/loadDBase.js');
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
 
-    await loadDB();
+    // await loadDB();
     //   const paises = ['argentina', 'venezuela', 'colombia'];
     //   paises.forEach(async (e) => await Country.create({name: e}))
   });
