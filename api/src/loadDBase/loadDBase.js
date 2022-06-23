9b57144766dc8f067c0f158fa81a08d0a1a5aec3
lucascencig <cenciglucas@gmail.com>
2022-06-22 21:06:47 -0300

    listo

--- api/src/loadDBase/loadDBase.js
+++ api/src/loadDBase/loadDBase.js
@@ -1,33 +1,31 @@
 const axios = require('axios');
 const { Country } = require('../db');
 
-
-
-const loadDB = async ()=> {
-  
+const LoadDB = async () => {
   try {
-    const apiInfo = (await axios.get("https://restcountries.com/v3/all")).data;
-    await apiInfo.map((element) => {
+    const apiInfo = (await axios.get('https://restcountries.com/v3/all')).data;
+    await apiInfo.map(element => {
       Country.findOrCreate({
         where: {
           idPais: element.cca3,
           name: element.name.common,
           imagen: element.flags[1],
           continente: element.continents[0],
-          capital: element.capital ? element.capital[0] : "Capital no encontrada",
+          capital: element.capital
+            ? element.capital[0]
+            : 'Capital no encontrada',
           subregion: element.subregion
             ? element.subregion
-            : "Subregion no encontrada",
+            : 'Subregion no encontrada',
           area: element.area,
-          poblacion: element.population
+          poblacion: element.population,
         },
       });
     });
-    return "Countries agregados a la base de datos.";
+    return 'Countries agregados a la base de datos.';
   } catch (e) {
-    console.log("/api/src/routes/apiInfo.js apiInfo error: " + e);
+    console.log('/api/src/routes/apiInfo.js apiInfo error: ' + e);
   }
 };
 
-
-module.exports = loadDB;
+module.exports = LoadDB;
